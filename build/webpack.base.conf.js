87fa86dec552c41bf40fc5f7f39dee901f6fff06
O1Dii <oldi730051@mail.ru>
2019-09-04 19:45:56 +0300

    Initial commit

---
+++ build/webpack.base.conf.js
@@ -0,0 +1,103 @@
+const path = require('path');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const PATHS = {
+  root: path.join(__dirname, '../'),
+  src: path.join(__dirname, '../src'),
+  project: path.join(__dirname, '../src/beer_project'),
+  dist: path.join(__dirname, '../dist'),
+  assets: 'assets/',
+};
+
+module.exports = {
+  externals: {
+    paths: PATHS,
+  },
+  entry: {
+    app: PATHS.src,
+  },
+  output: {
+    filename: `${PATHS.assets}js/[name].[hash].js`,
+    path: PATHS.dist,
+    publicPath: '/',
+  },
+  optimization: {
+    splitChunks: {
+      cacheGroups: {
+        vendor: {
+          name: 'vendors',
+          test: /node_modules/,
+          chunks: 'all',
+          enforce: true,
+        },
+      },
+    },
+  },
+  module: {
+    rules: [
+      {
+        test: /\.j(s|sx)$/,
+        loader: 'babel-loader',
+        exclude: '/node_modules/',
+        query: {
+          presets: ['@babel/env', '@babel/react'],
+          plugins: [
+            ['@babel/plugin-proposal-decorators', { legacy: true }],
+            ['@babel/plugin-proposal-class-properties', { loose: true }],
+          ],
+        },
+      },
+      {
+        test: /\.(png|jpg|gif|svg)$/,
+        loader: 'file-loader',
+        options: {
+          name: '[name].[ext]',
+        },
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          'style-loader',
+          {
+            loader: 'css-loader',
+            options: { sourceMap: true },
+          },
+          {
+            loader: 'postcss-loader',
+            options: { sourceMap: true, config: { path: `${PATHS.root}/postcss.config.js` } },
+          },
+          {
+            loader: 'sass-loader',
+            options: { sourceMap: true },
+          },
+        ],
+      },
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          {
+            loader: 'css-loader',
+            options: { sourceMap: true },
+          },
+          {
+            loader: 'postcss-loader',
+            options: { sourceMap: true, config: { path: `${PATHS.root}/postcss.config.js` } },
+          },
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: `${PATHS.src}/index.html`,
+      filename: './index.html',
+      inject: true,
+    }),
+    new CopyWebpackPlugin([{ from: `${PATHS.project}/img`, to: `${PATHS.assets}img` }]),
+  ],
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  },
+};
